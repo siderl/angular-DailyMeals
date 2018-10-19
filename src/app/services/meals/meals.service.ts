affac39a6d8aa88bd952f99508e27fa2219db938
Sahid Michel <siderl@MacBook-Pro-de-Miroslava.local>
2018-10-19 01:44:45 -0500

    pre evaluation front stuff

---
+++ src/app/services/meals/meals.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+
+
+@Injectable()
+export class MealsService {
+
+  private generateDietURL = 'http://18.225.18.62:8080/DailyMeals/ServiceDailyMeals.svc/GetData/';
+  constructor(private http: Http) { }
+
+  public getMeals(kCalories){
+    const headers = new Headers();
+    headers.append('content-type', 'application/x-www-form-urlencoded');
+
+    return this.http.get(
+      this.generateDietURL + kCalories,
+      {headers: headers});
+  }
+}
